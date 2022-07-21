41cbee188a3d447e2d310797effc82cdd87dc46f
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-07-21 17:30:50 +0200

    first commit

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+use App\Post;
+use App\Category;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+
+        return view('admin.posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::all();
+
+        return view('admin.posts.create', compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required|string|max:65535',
+            'published' => 'sometimes|accepted',
+            'category_id' => 'nullable|exists:categories,id'
+        ]);
+
+        $data = $request->all();
+        $newPost = new Post();
+        $newPost->fill($data);
+
+        $newPost->slug = $this->getSlug($data['title']);
+
+        $newPost->published = isset($data['published']);
+        $newPost->save();
+
+        return redirect()->route('admin.posts.show', $newPost->id);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return view('admin.posts.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        $categories = Category::all();
+
+        return view('admin.posts.edit', compact('post', 'categories'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required|string|max:65535',
+            'published' => 'sometimes|accepted',
+            'category_id' => 'nullable|exists:categories,id'
+        ]);
+
+        $data = $request->all();
+
+        if( $post->title != $data['title'] ) {
+            $post->slug = $this->getSlug($data['title']);
+        }
+        $post->fill($data);
+
+        $post->published = isset($data['published']);
+
+        $post->save();
+
+        return redirect()->route('admin.posts.show', $post->id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+        return redirect()->route('admin.posts.index');
+    }
+
+    private function getSlug($title)
+    {
+        $slug = Str::of($title)->slug('-');
+        $count = 1;
+
+        while( Post::where('slug', $slug)->first() ) {
+            $slug = Str::of($title)->slug('-') . "-{$count}";
+            $count++;
+        }
+
+        return $slug;
+    }
+}
---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h1>Crea un nuovo post</h1>
+            </div>
+            <div class="card-body">
+                <form action="{{route('admin.posts.store')}}" method="POST">
+                    @csrf
+                    <div class="form-group">
+                        <label for="title">Titolo</label>
+                        <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{old('title')}}">
+                        @error('title')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="content">Contenuto</label>
+                        <textarea class="form-control @error('content') is-invalid @enderror" id="content" name="content" rows="10">{{old('content')}}</textarea>
+                        @error('content')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="category">Categoria</label>
+                        <select class="form-control @error('category_id') is-invalid @enderror" id="category" name="category_id">
+                            <option value="">Seleziona Categoria</option>
+                            @foreach ($categories as $category)
+                                <option value="{{$category['id']}}" {{old('category_id') == $category['id'] ? 'selected' : ''}}>{{$category['name']}}</option>
+                            @endforeach
+                        </select>
+                        @error('category_id')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="form-group form-check">
+                        <input type="checkbox" class="form-check-input @error('published') is-invalid @enderror" id="published" name="published" {{old('published') ? 'checked' : ''}}>
+                        <label class="form-check-label" for="published">Pubblica</label>
+                        @error('published')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <button type="submit" class="btn btn-primary">Crea</button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
